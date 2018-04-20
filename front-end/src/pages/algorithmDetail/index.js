7a0cf84366c6d921815828496b6df658375373a0
tolgaatam <ktakamiltolga@gmail.com>
2018-04-20 21:40:21 +0300

    algorithm detail page visual fixezzzzz

--- front-end/src/pages/algorithmDetail/index.js
+++ front-end/src/pages/algorithmDetail/index.js
@@ -90,8 +90,8 @@ class Home extends Component {
 					<Row className="clear-from-top">
 						<Col xs={12} sm={12} md={12} lg={12}>
 							<ListGroup>
-								<ListGroupItem className="slightly-bigger-text">{description}</ListGroupItem>
-								<ListGroupItem className="slightly-bigger-text">{synopsis}</ListGroupItem>
+								<ListGroupItem className="slightly-bigger-text"><strong>Synopsis:</strong><br>{synopsis}</ListGroupItem>
+								<ListGroupItem className="slightly-bigger-text"><strong>Description:</strong><br>{description}</ListGroupItem>
 							</ListGroup>
 						</Col>
 					</Row>
